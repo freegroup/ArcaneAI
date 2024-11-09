bb92b29c304a0e72a6312e3f2944303bf324a00c
Herz, Andreas <1155039+freegroup@users.noreply.github.com>
2024-11-09 19:41:27 +0100

    ...

--- game/src/stt/factory.py
+++ game/src/stt/factory.py
@@ -1,5 +1,5 @@
-from stt.whisper_local import WhisperLocal
-from stt.whisper_openai import WhisperOpenAi
+#from stt.whisper_local import WhisperLocal
+#from stt.whisper_openai import WhisperOpenAi
 from stt.cli_text import CLIText
 
 class STTFactory:
