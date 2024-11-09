36e131fa2125964a351f27b1a4bd4534faa0e3e1
Herz, Andreas <1155039+freegroup@users.noreply.github.com>
2024-11-09 19:54:25 +0100

    ..

--- game/src/server.py
+++ game/src/server.py
@@ -237,6 +237,6 @@ async def websocket_endpoint(websocket: WebSocket, token: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=9000)
+    uvicorn.run(app, host="127.0.0.1", port=9000, forwarded_allow_ips="*")
 
 
